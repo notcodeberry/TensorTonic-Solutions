6843c851dc20e34cf62d6af640832185a3751310
Aleksander Mucha <78071518+notcodeberry@users.noreply.github.com>
2026-03-29 00:04:29 +0100

    Update solution for transformers-positional-encoding

--- transformer/transformers-positional-encoding/transformers-positional-encoding.py
+++ transformer/transformers-positional-encoding/transformers-positional-encoding.py
@@ -4,11 +4,12 @@ def positional_encoding(seq_length: int, d_model: int) -> np.ndarray:
     """
     Generate sinusoidal positional encodings.
     """
-    
-    pos_encodings = np.zeros((seq_length, d_model))
-    
+
     # Your code here
 
+    """
+    pos_encodings = np.zeros((seq_length, d_model))
+    
     for pos in range(seq_length):
         for i in range(d_model):
             if i % 2 == 0:
@@ -18,3 +19,17 @@ def positional_encoding(seq_length: int, d_model: int) -> np.ndarray:
             pos_encodings[pos][i] = pe
 
     return pos_encodings 
+    """
+    
+    pos = np.arange(seq_length).reshape(seq_length,1)
+    i = np.arange(d_model).reshape(1,d_model)
+
+    angle_rates = 1 / (10000**((2 * (i//2)) / d_model))
+    angles = pos * angle_rates #(seq_len, d_model)
+
+    pe = np.zeros((seq_length, d_model))
+
+    pe[:, 0::2] = np.sin(angles[:, 0::2])
+    pe[:, 1::2] = np.cos(angles[:, 1::2])
+
+    return pe
