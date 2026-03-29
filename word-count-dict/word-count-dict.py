0d29249235c1eb2cc8cc932245c2c5462b97c137
Aleksander Mucha <78071518+notcodeberry@users.noreply.github.com>
2026-03-29 15:14:04 +0200

    Add solution for word-count-dict

---
+++ word-count-dict/word-count-dict.py
@@ -0,0 +1,16 @@
+def word_count_dict(sentences):
+    """
+    Returns: dict[str, int] - global word frequency across all sentences
+    """
+    # Your code here
+    dict = {}
+
+    for sent in sentences:
+        for word in sent:
+            if word in dict:
+                i = dict[word]
+                dict[word] = i + 1 
+            else:
+                dict[word] = 1 
+
+    return dict
